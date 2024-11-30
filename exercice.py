a5b9e8598cfb2af437fa85b0a011581c6c3d38e9
Brendan <pidouxbrendan@gmail.com>
2024-11-30 10:13:36 +0100

    # create todo

--- exercice.py
+++ exercice.py
@@ -1,40 +1,15 @@
-def main():
-    tasks = []
-    while True:
-	print("\n==== To-Do List ====")
-	print("1. Add Task")
-	print("2. Show Tasks")
-	print("3. Mark Task as Done")
-	print("4. Exit")
+# Liste pour stocker les todos
+todos = []
 
-	choice = input("Enter your choice: ")
-
-    if choice == '1':
-	print()
-	n_tasks = int(input("How may task you want to add:"))
-
-	for i in range(n_tasks):
-	    task = input("Enter the task:")
-	    tasks.append({"task": task, "done": False})
-
-    elif choice == '2':
-	print("\nTasks:")
-	for index, task in enumerate(tasks):
-	    status = "Done" if task["done"] else "Not Done"
-	    print(f"{index + 1}. {task['task']} - {status}")
-
-    elif choice == '3':
-	task_index = int(input("Enter the task to mark as done: ")) - 1
-	if 0 <= task_index < len(tasks):
-	    tasks[task_index]["done"] =  True
-	    print ("Task marked as done!")
+# Focntion pour lister les todos
+def lister_todos():
+	print("\n==== Liste des todos ====")
+	if not todos:
+		print("Aucun todo pour le moment.")
 	else:
-	    print("Invalid task number")
+		for i, todo in enumerate(todos, start=1):
+			statut = "O" if todo[fait] else "X"
+			print(f"{i}. {todo['titre']} - {statut}")
+	print("===========================")
 
-    elif choice '4':
-	print("Exiting the To-Do List.")
-	break
-    else:
-	print("Invalid choice. Please try again.")
-if __name__ == "__main__":
-	main()
+
